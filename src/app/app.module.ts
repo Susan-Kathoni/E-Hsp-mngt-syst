3de8a5363e7225357ee869586bcbbbd90feb5084
Susan-Kathoni <123joey4kathoniey@gmail.com>
2021-01-14 03:37:14 +0300

    Create homepage layout.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomepageComponent,
+    AboutComponent,
+    ServicesComponent,
+    
   ],
   imports: [
     BrowserModule,
